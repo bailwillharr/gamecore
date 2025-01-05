c7cd48ce760be849abf4b97f01f8fb5ae3dc3166
bailwillharr <baileyharrison04@outlook.com>
2025-01-05 01:26:13 +0000

    Change getContentDir() to use SDL function

--- gamecore/src/gc_disk_io.cpp
+++ gamecore/src/gc_disk_io.cpp
@@ -5,9 +5,7 @@
 #include <filesystem>
 #include <optional>
 
-#ifdef WIN32
-#include <Windows.h>
-#endif
+#include <SDL3/SDL_filesystem.h>
 
 #include "gamecore/gc_assert.h"
 #include "gamecore/gc_logger.h"
@@ -16,68 +14,19 @@ namespace gc {
 
 std::optional<std::filesystem::path> findContentDir()
 {
-#ifdef WIN32
-    // get resource path relative to exe directory if on windows
-
-    // Copied from github.com/libsdl-org/SDL
-    // src/filesystem/windows/SDL_sysfilesystem.c
-    // SDL_SYS_GetBasePath(void)
-
-    DWORD buflen = 128;
-    WCHAR* path = NULL;
-    std::filesystem::path content_dir{};
-    DWORD len = 0;
-    int i;
-
-    while (true) {
-        void* ptr = realloc(path, buflen * sizeof(WCHAR));
-        if (!ptr) {
-            free(path);
-            return {};
-        }
-
-        path = (WCHAR*)ptr;
-
-        len = GetModuleFileNameW(NULL, path, buflen);
-        // if it truncated, then len >= buflen - 1
-        // if there was enough room (or failure), len < buflen - 1
-        if (len < buflen - 1) {
-            break;
-        }
-
-        // buffer too small? Try again.
-        buflen *= 2;
-    }
-
-    if (len == 0) {
-        free(path);
-        GC_ERROR("Couldn't locate our .exe");
-        return {};
-    }
-
-    for (i = len - 1; i > 0; i--) {
-        if (path[i] == '\\') {
-            break;
-        }
-    }
-
-    GC_ASSERT(i > 0);   // Should have been an absolute path.
-    path[i + 1] = '\0'; // chop off filename.
-
-    content_dir = std::filesystem::path(path);
-    free(path);
-#else
-    // for other OS's use current directory ._.
-    std::filesystem::path content_dir = std::filesystem::current_path();
-#endif
-    content_dir /= "content";
-    if (std::filesystem::is_directory(content_dir) == false) {
-        GC_ERROR("Unable to find game resources directory");
-        return {};
-    }
-    else {
-        return content_dir;
-    }
+	const char* const base_path = SDL_GetBasePath();
+	if (base_path) {
+		const std::filesystem::path content_dir = std::filesystem::path(base_path) / "content";
+    	if (std::filesystem::is_directory(content_dir)) {
+			return content_dir;
+		} else {
+			GC_ERROR("Failed to find content dir: {} is not a directory", content_dir.string());
+			return {};
+		}
+	} else {
+		GC_ERROR("Failed to find content dir: SDL_GetBasePath() error: {}", SDL_GetError());
+		return {};
+	}
 }
 
 } // namespace gc
