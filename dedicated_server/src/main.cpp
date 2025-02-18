15f2fb349db64827f1ffbfd990598e3579d71617
bailehuni <bailey.harrison@durham.ac.uk>
2025-02-18 22:39:19 +0000

    figure out asio

--- dedicated_server/src/main.cpp
+++ dedicated_server/src/main.cpp
@@ -1,33 +1,87 @@
+#include <string>
+#include <iostream>
+#include <span>
+
 #include <asio.hpp>
 
 #include <gamecore/gc_app.h>
-#include <gamecore/gc_abort.h>
+#include <gamecore/gc_window.h>
+#include <gamecore/gc_vulkan_renderer.h>
 
-static void on_connect(const asio::error_code& ec)
-{
-	GC_INFO("Async connected! ec: {}", ec.message());
-}
+// an echo server
 
-static void network_test()
+constexpr asio::ip::port_type SERVER_PORT = 1234;
+
+static asio::awaitable<void> echo()
 {
-	asio::error_code ec;
-	asio::io_context context;
-	asio::ip::tcp::socket socket(context);
-	const asio::ip::address address = asio::ip::make_address("10.0.0.101", ec);
-	if (ec) {
-		gc::abortGame("asio::ip::make_address() error: {}", ec.message());
-	}
-	const asio::ip::port_type port = 80;
-	const asio::ip::tcp::endpoint endpoint(address, port);	
-	socket.async_connect(endpoint, on_connect);
-	context.run();
+    /* get io_context for this coroutine (passed to co_spawn) */
+    const asio::any_io_executor ctx = co_await asio::this_coro::executor;
+
+    /* Get endpoint for the server (bound to 0.0.0.0 port 1234) */
+    const asio::ip::tcp::endpoint server_endpoint(asio::ip::tcp::v4(), SERVER_PORT);
+
+    for (;;) {
+        /* acceptor object can asynchronously wait for a client to connect */
+        asio::ip::tcp::acceptor acceptor(ctx, server_endpoint);
+
+        std::cout << "Waiting for client to connect...\n";
+
+        /* returns a socket to communicate with a client */
+        asio::ip::tcp::socket sock = co_await acceptor.async_accept(ctx, asio::use_awaitable);
+
+        std::cout << "Connected!\n";
+
+        while (sock.is_open()) {
+            std::array<char, 512> buf{};
+            const size_t sz = co_await sock.async_read_some(asio::buffer(buf.data(), buf.size()), asio::use_awaitable);
+
+            std::cout << "Received " << sz << " bytes of data\n";
+
+            std::cout << "Received string: " << std::string_view(buf.begin(), buf.end()) << "\n";
+
+            size_t bytes_remaining = sz;
+            while (bytes_remaining > 0) {
+                const size_t bytes_written =
+                    co_await sock.async_write_some(asio::buffer(buf.data() + sz - bytes_remaining, bytes_remaining), asio::use_awaitable);
+                std::cout << "Wrote " << bytes_written << " bytes\n";
+                bytes_remaining -= bytes_written;
+            }
+        }
+
+        std::cout << "Closed connection\n";
+    }
 }
 
 int main()
 {
     gc::App::initialise();
 
-	network_test();
+    asio::io_context ctx;
+    co_spawn(ctx, echo(), asio::detached);
 
+    gc::app().window().setWindowVisibility(true);
+
+    while (!gc::app().window().shouldQuit()) {
+
+        gc::app().vulkanRenderer().waitForRenderFinished();
+
+        gc::app().window().processEvents();
+
+        if (gc::app().window().getKeyPress(SDL_SCANCODE_F11)) {
+            gc::app().window().setSize(0, 0, !gc::app().window().getIsFullscreen());
+        }
+        if (gc::app().window().getKeyPress(SDL_SCANCODE_ESCAPE)) {
+            gc::app().window().setQuitFlag();
+        }
+
+        auto count = ctx.poll();
+        if (count > 0) {
+            std::cout << "Poll ran " << count << " handlers\n";
+        }
+
+        gc::app().vulkanRenderer().acquireAndPresent({});
+    }
+
+    // ctx.run(); // block until echo() has returned
     gc::App::shutdown();
 }
