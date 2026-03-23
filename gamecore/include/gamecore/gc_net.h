ecde6e9550c1363e02524b549cd8bb096eca25fc
bailwillharr <baileyharrison04@outlook.com>
2026-03-23 13:08:05 +0000

    Change to UDP server

--- gamecore/include/gamecore/gc_net.h
+++ gamecore/include/gamecore/gc_net.h
@@ -3,12 +3,26 @@
 #include <thread>
 #include <memory>
 #include <queue>
+#include <unordered_map>
 
+#include <asio/ip/udp.hpp>
 #include <asio/io_context.hpp>
 
 #include "gamecore/gc_name.h"
 
 namespace gc {
+
+constexpr size_t NET_USERNAME_MAX_CHARS = 15;
+
+struct NetSession {
+    char username[NET_USERNAME_MAX_CHARS + 1];
+};
+
+struct NetServer {
+    std::jthread server_thread{};
+    std::mutex sessions_mutex{};
+    std::unordered_map<asio::ip::udp::endpoint, NetSession> sessions{};
+};
 
 struct NetEvent {
     Name type;
@@ -26,20 +40,18 @@ public:
 };
 
 class Net {
-    std::shared_ptr<asio::io_context> m_context{};
-    std::shared_ptr<NetEventQueue> m_event_queue{};
-    std::jthread m_server_thread{};
+    asio::io_context m_context{};
+    NetEventQueue m_event_queue{};
+    NetServer m_server{};
 
 public:
     Net();
 
     void startServer();
-    void stopServer();
+    void stop();
 
     // returns true if an event is available
     bool pollEvents(NetEvent& ev);
-
-    bool isServerRunning() const;
 };
 
 } // namespace gc
--- gamecore/src/gc_net.cpp
+++ gamecore/src/gc_net.cpp
@@ -2,13 +2,11 @@
 
 #include <asio/error.hpp>
 #include <asio/io_context.hpp>
-#include <string_view>
-#include <memory>
 #include <thread>
 
 #include <asio/awaitable.hpp>
 #include <asio/ip/address_v4.hpp>
-#include <asio/ip/tcp.hpp>
+#include <asio/ip/udp.hpp>
 #include <asio/steady_timer.hpp>
 #include <asio/this_coro.hpp>
 #include <asio/use_awaitable.hpp>
@@ -25,123 +23,49 @@
 
 namespace gc {
 
-static asio::awaitable<void> clientSession(asio::ip::tcp::socket socket, std::shared_ptr<NetEventQueue> event_queue)
-{
-    using namespace ::gc::literals;
-
-    constexpr auto token = asio::as_tuple(asio::use_awaitable);
-
-    asio::error_code ec{};
-
-    auto remote = socket.remote_endpoint(ec);
-    if (ec) {
-        GC_ERROR("Error getting remote endpoint: {}", ec.message());
-        co_return;
-    }
-    GC_INFO("New session: {}:{}", remote.address().to_string(), remote.port());
-    const auto hello_message = std::format("Connected to server. Your IP: {}:{}\n", remote.address().to_string(), remote.port());
-    std::tie(ec, std::ignore) = co_await asio::async_write(socket, asio::buffer(hello_message), token);
-    if (ec) {
-        GC_ERROR("Write error: {}", ec.message());
-        co_return;
-    }
-
-    size_t bytes_read = 0;
-    std::vector<char> data(1024);
-
-    while (true) {
-        asio::streambuf buf;
-        std::tie(ec, bytes_read) = co_await asio::async_read_until(socket, buf, '\n', token);
-        if (ec) {
-            if (ec != asio::error::eof) {
-                GC_ERROR("Read error: {}", ec.message());
-            }
-            else {
-                GC_INFO("Client sent EOF");
-            }
-            break;
-        }
-
-        const std::string_view line(static_cast<const char*>(buf.data().data()), buf.size());
-        buf.consume(buf.size());
-
-        if (line == "exit\n") {
-            const std::string exit_message = "Goodbye\n";
-            std::tie(ec, std::ignore) = co_await asio::async_write(socket, asio::buffer(exit_message), token);
-            if (ec) {
-                GC_ERROR("Write error: {}", ec.message());
-                break;
-            }
-
-            socket.shutdown(asio::ip::tcp::socket::shutdown_send, ec);
-            if (ec) {
-                GC_ERROR("Failed to call socket.shutdown(): {}", ec.message());
-            }
-
-            break;
-        }
-        else if (line == "shutdown\n") {
-            event_queue->push(NetEvent{"shutdown"_name});
-        }
-    }
-
-    GC_INFO("Closing session: {}:{}", remote.address().to_string(), remote.port());
-
-    // socket will go out of scope and close here
-}
-
-static asio::awaitable<void> waitForConnections(asio::ip::tcp::endpoint endpoint, std::shared_ptr<NetEventQueue> event_queue)
+static asio::awaitable<void> serverLoop(asio::ip::udp::endpoint endpoint)
 {
     constexpr auto token = asio::as_tuple(asio::use_awaitable);
 
     asio::error_code ec{};
     auto executor = co_await asio::this_coro::executor;
 
-    asio::ip::tcp::acceptor acceptor(executor);
+    asio::ip::udp::socket socket(executor);
 
-    acceptor.open(endpoint.protocol(), ec);
+    socket.open(endpoint.protocol(), ec);
     if (ec) {
-        GC_ERROR("Failed to open acceptor: {}", ec.message());
+        GC_ERROR("Failed to open socket: {}", ec.message());
         co_return;
     }
 
-    constexpr int MAX_BIND_ATTEMPTS = 3;
-    for (int i = 0; i < MAX_BIND_ATTEMPTS; ++i) {
-        acceptor.bind(endpoint, ec);
-        if (ec) {
-            if (ec == asio::error::address_in_use) {
-                GC_WARN("Address in use: {}:{}", endpoint.address().to_string(), endpoint.port());
-                endpoint = asio::ip::tcp::endpoint(endpoint.address(), endpoint.port() + 1);
-                continue;
-            }
-            else {
-                GC_ERROR("Failed to bind acceptor: {}", ec.message());
-                co_return;
-            }
-        }
-        else {
-            break;
-        }
+    socket.bind(endpoint, ec);
+    if (ec) {
+        GC_ERROR("Failed to bind socket: {}", ec.message());
+        co_return;
     }
 
     GC_INFO("Starting server on {}:{}", endpoint.address().to_string(), endpoint.port());
 
-    acceptor.listen(acceptor.max_listen_connections, ec);
-    if (ec) {
-        GC_ERROR("Failed to listen on acceptor: {}", ec.message());
-        co_return;
-    }
-
     for (;;) {
-        asio::ip::tcp::socket socket(executor);
-
-        std::tie(ec) = co_await acceptor.async_accept(socket, token);
+        size_t bytes_read{};
+        std::array<char, 8> buf{};
+        asio::ip::udp::endpoint client_endpoint{};
+        std::tie(ec, bytes_read) = co_await socket.async_receive_from(asio::buffer(buf), client_endpoint, token);
         if (ec) {
-            GC_ERROR("Acceptor failed to accept: {}", ec.message());
+            GC_ERROR("Failed to receive on socket: {}", ec.message());
             continue;
         }
 
-        asio::co_spawn(executor, clientSession(std::move(socket), event_queue), asio::detached);
+        size_t bytes_written{};
+        auto send_buf = std::format("You sent {} bytes", bytes_read);
+        std::tie(ec, bytes_written) = co_await socket.async_send_to(asio::buffer(send_buf), client_endpoint, token);
+        if (ec) {
+            GC_ERROR("Failed to send on socket: {}", ec.message());
+            continue;
+        }
+        if (bytes_written < send_buf.size()) {
+            GC_ERROR("Didn't send all data. Sent {}/{}", bytes_written, send_buf.size());
+        }
     }
 }
 
@@ -164,23 +88,21 @@ bool NetEventQueue::pop(NetEvent& ev)
     }
 }
 
-Net::Net() : m_context(std::make_shared<asio::io_context>()), m_event_queue(std::make_shared<NetEventQueue>()) {}
+Net::Net() {}
 
 void Net::startServer()
 {
-    m_server_thread = std::jthread(
-        [](std::shared_ptr<asio::io_context> context, std::shared_ptr<NetEventQueue> event_queue) {
-            asio::ip::tcp::endpoint endpoint(asio::ip::address(), 6969);
-            asio::co_spawn(*context, waitForConnections(endpoint, event_queue), asio::detached);
+    m_server.server_thread = std::jthread(
+        [](asio::io_context* context) {
+            asio::ip::udp::endpoint endpoint(asio::ip::address(), 6969);
+            asio::co_spawn(*context, serverLoop(endpoint), asio::detached);
             context->run();
         },
-        m_context, m_event_queue);
+        &m_context);
 }
 
-void Net::stopServer() { m_context->stop(); }
+void Net::stop() { m_context.stop(); }
 
-bool Net::pollEvents(NetEvent& ev) { return m_event_queue->pop(ev); }
-
-bool Net::isServerRunning() const { return !m_context->stopped(); }
+bool Net::pollEvents(NetEvent& ev) { return m_event_queue.pop(ev); }
 
 } // namespace gc
